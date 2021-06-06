04ca4643560f27d2240f3053e3cd22456fa8b682
Floffah <therealfloffah@gmail.com>
2021-06-06 10:17:23 +0100

    ts err

--- src/components/display/ListedPatchNote.tsx
+++ src/components/display/ListedPatchNote.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import Embed from "./Embed";
 import { LPNIcons, LPNTitle, LPNVersion } from "./ListedPatchNote.styles";
 import { StyledIcon } from "../util/StyledIcon";
-import { mdiArrowDown, mdiArrowRight, mdiChevronDown } from "@mdi/js";
+import { mdiArrowRight, mdiChevronDown } from "@mdi/js";
 
 export interface ListedPatchNoteProps {
     title: string;
